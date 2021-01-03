1887a9d32c922586a1a850a25792f4dab0170c07
Chatchat Paisalpanich <frinttropystudy222@gmail.com>
2021-01-03 16:41:20 +0700

    Renamed function in main.py

--- main.py
+++ main.py
@@ -6,23 +6,25 @@ This is a program/project for drawing stuff using turtle
 but more importantly for learning git and github...
 """
 
-def square(t, length):
+def draw_square(t, length):
     # t stands for turtle
     for _ in range(4):
         t.forward(length)
         t.right(90)
 
-def triangle(t, length):
+
+def draw_triangle(t, length):
     # t stands for turtle
     # This func is another dev say "CoolDev69"
     for _ in range(3):
         t.forward(length)
         t.right(120)
-   
-def polygon(t, length, sides):
+
+
+def draw_polygon(t, length, sides):
     internal_angle = ((sides-2) * 180) / sides
     external_angle = 180 - internal_angle
     for _ in range(sides):
         t.forward(length)
         t.right(external_angle)
-    
+
