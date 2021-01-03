103aab9bb791defa6d0984d2f1c2021ea1d13dc0
Chatchat Paisalpanich <frinttropystudy222@gmail.com>
2021-01-03 16:03:49 +0700

    Added 'square' function to main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import math
+import turtle as tur
 
-print("Hello world")
+"""
+This is a program/project for drawing stuff using turtle
+but more importantly for learning git and github...
+"""
+
+def square(t, length):
+    # t stands for turtle
+    for _ in range(4):
+        t.forward(length)
+        t.right(90)
+
+
